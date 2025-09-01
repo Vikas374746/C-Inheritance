5af1f9d15ef18ad1fa5d6a8971a6cdb53a66036d
Vikas Jayavarapu <vikasjayavarapu12@gmail.com>
2025-09-01 14:57:53 +0530

    Create exp_1.cpp

---
+++ exp_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+public:
+    string brand = "Ford";
+    void color() {
+        cout << "Red!\n";
+    }
+};
+
+// Derived class 
+class Car : public Vehicle {
+public:
+    string model = "Mustang";
+};
+
+int main() {
+    Car myCar;
+    myCar.color();
+    cout << myCar.brand + " " + myCar.model;
+    return 0;
+}
+
+/*
+Output:
+Red!
+Ford Mustang
+*/
